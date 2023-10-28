84c797b58311289cc39209095db5d0f3140d1715
hfdzz <yhafidz@gmail.com>
2023-10-28 11:35:42 +0700

    init

---
+++ uts-frontend/src/main.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const test = async () => {
+      const response = await fetch('http://127.0.0.1:6543/test')
+      const data = await response.json()
+      console.log(data)
+    }
+    test();
+  }, []);
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
